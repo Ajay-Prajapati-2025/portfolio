a714fd4f81e23669795e954a0f23736eefbd0b0d
Ajay Prajapati <prajapatiajayrameshbhai@gmail.com>
2024-07-05 21:21:16 +0530

    Updated content

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -3,20 +3,26 @@ import about from "../assets/about.jpg";
 
 export default function About() {
   return (
-    <div className="text-white max-w-6xl mx-auto my-12 flex flex-col gap-7 py-10" id="about">
-        <div className="text-center mb-5">
-          <h1 className="text-4xl font-bold mb-4 primary-color ">About Me</h1>
-        </div>
+    <div
+      className="text-white max-w-6xl mx-auto my-12 flex flex-col gap-7 py-10"
+      id="about"
+    >
+      <div className="text-center mb-5">
+        <h1 className="text-4xl font-bold mb-4 primary-color ">About Me</h1>
+      </div>
       <div className="md:grid md:grid-cols-2 s:py-16">
-
         <div className="mt-4 md:mt-0 text-left flex">
           <div className="my-auto mx-6 ">
-            <h2 className="text-4xl font-bold mb-5 primary-color">Ajay Prajapati</h2>
+            <h2 className="text-4xl font-bold mb-5 primary-color">
+              Ajay Prajapati
+            </h2>
             <p className="text-xl lg:text-lg tracking-wide mt-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Reprehenderit rem, perspiciatis, iure sequi perferendis voluptates
-              quos fugit minus eum accusamus quidem sapiente veniam vel aliquam
-              qui blanditiis adipisci aliquid. 
+              I am a proficient MERN stack developer with a robust understanding
+              of MongoDB, Express.js, React, and Node.js. She excels in creating
+              dynamic and responsive web applications, leveraging her expertise
+              in front-end and back-end development. Additionally, Yesha
+              possesses strong programming skills(JAVA, JAVA SCRIPT etc.),
+              enhancing her ability to deliver efficient and scalable solutions.
             </p>
           </div>
         </div>
@@ -26,9 +32,7 @@ export default function About() {
           alt="About me"
           className="mx-auto rounded-3xl py-8 md:py-0 w-96 shadow-lg shadow-white"
         />
-
       </div>
-
     </div>
   );
 }
--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -17,15 +17,15 @@ export default function Projects() {
       <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-x-7 gap-y-7">
 
         <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group  rounded-md flex justify-center items-center   h-[200px] bg-cover relative">
-          <img src={proj1} alt="Project 1" />
+          <img src="https://images.unsplash.com/photo-1517292987719-0369a794ec0f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmVhbCUyMEV0YXN0ZSUyMGJvb2tpbmclMjBhcHB8ZW58MHwwfDB8fHww" alt="Project 1" />
 
           {/* Live effect */}
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">Project</span>
+            <span className="text-2xl font-bold text-white tracking-wider">MittiHomes</span>
             <div className="pt-8 text-center">
-              <a href="/">
+              <a href="https://github.com/Ajay-Prajapati-2025">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gary-700 font-bold text-lg">
-                  Live
+                  Know More
                 </button>
               </a>
             </div>
@@ -33,15 +33,15 @@ export default function Projects() {
         </div>
 
         <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group  rounded-md flex justify-center items-center   h-[200px] bg-cover relative">
-          <img src={proj2} alt="Project 1" />
+          <img src="https://images.unsplash.com/photo-1423666639041-f56000c27a9a?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Project 1" />
 
           {/* Live effect */}
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">Project</span>
+            <span className="text-2xl font-bold text-white tracking-wider">Blog App</span>
             <div className="pt-8 text-center">
-              <a href="/">
+              <a href="https://github.com/Ajay-Prajapati-2025">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gary-700 font-bold text-lg">
-                  Live
+                Know More
                 </button>
               </a>
             </div>
@@ -49,15 +49,15 @@ export default function Projects() {
         </div>
 
         <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group  rounded-md flex justify-center items-center   h-[200px] bg-cover relative">
-          <img src={proj1} alt="Project 1" />
+          <img src="https://images.unsplash.com/photo-1606326608606-aa0b62935f2b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Project 1" />
 
           {/* Live effect */}
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">Project</span>
+            <span className="text-2xl font-bold text-white tracking-wider">Quiz Generator</span>
             <div className="pt-8 text-center">
-              <a href="/">
+              <a href="https://github.com/Ajay-Prajapati-2025">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gary-700 font-bold text-lg">
-                  Live
+                Know More
                 </button>
               </a>
             </div>
@@ -65,15 +65,15 @@ export default function Projects() {
         </div>
 
         <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg sshadow-[#040c16] group  rounded-md flex justify-center items-center   h-[200px] bg-cover relative">
-          <img src={proj3} alt="Project 1" />
+          <img src="https://images.unsplash.com/photo-1608111283390-2e333b9b279c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Project 1" />
 
           {/* Live effect */}
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">Project</span>
+            <span className="text-2xl font-bold text-white tracking-wider">Tic-Tac-Toe</span>
             <div className="pt-8 text-center">
-              <a href="/">
+              <a href="https://github.com/Ajay-Prajapati-2025">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gary-700 font-bold text-lg">
-                  Live
+                Know More
                 </button>
               </a>
             </div>
@@ -81,15 +81,15 @@ export default function Projects() {
         </div>
 
         <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group  rounded-md flex justify-center items-center   h-[200px] bg-cover relative">
-          <img src={proj4} alt="Project 1" />
+          <img src="https://images.unsplash.com/photo-1567606404787-ebd010176fdf?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Project 1" />
 
           {/* Live effect */}
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">Project</span>
+            <span className="text-2xl font-bold text-white tracking-wider">Netflix-clone</span>
             <div className="pt-8 text-center">
-              <a href="/">
+              <a href="https://github.com/Ajay-Prajapati-2025">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gary-700 font-bold text-lg">
-                  Live
+                Know More
                 </button>
               </a>
             </div>
@@ -97,23 +97,21 @@ export default function Projects() {
         </div>
 
         <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group  rounded-md flex justify-center items-center   h-[200px] bg-cover relative">
-          <img src={proj5} alt="Project 1" />
+          <img src="https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Project 1" />
 
           {/* Live effect */}
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">Project</span>
+            <span className="text-2xl font-bold text-white tracking-wider">Simon-game</span>
             <div className="pt-8 text-center">
-              <a href="/">
+              <a href="https://github.com/Ajay-Prajapati-2025">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gary-700 font-bold text-lg">
-                  Live
+                Know More
                 </button>
               </a>
             </div>
           </div>
         </div>
 
-
-        
       </div>
     </div>
   );
